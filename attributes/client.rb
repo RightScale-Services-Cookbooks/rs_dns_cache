f6637ccd2afe7095900885b2e12ccf797a482fd9
Richard Shade <rshade@rightscale.com>
2014-02-27 16:21:55 -0600

    ading amazonaws

--- attributes/client.rb
+++ attributes/client.rb
@@ -1,6 +1,9 @@
 case node[:cloud][:provider]
 when "ec2"
-  default[:resolver][:search]+=" ec2.internal"
+  default[:resolver][:search]+=" ec2.internal us-west-2.compute.internal"
+  default[:resolver][:nameserver]<<"172.16.0.23"
 when "google"
   Chef::Log.info "google"
+  default[:resolver][:search]+=" google.internal"
+  default[:resolver][:nameserver]<<"169.254.169.254"
 end
